56c2477c7820788e2e883d31b3421c470612ac5d
sammyndu <48829214+sammyndu@users.noreply.github.com>
2020-01-17 12:32:58 +0100

    added msssql conn

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace EManager3
             else{
                 services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("MSSQLConnection")));
 //                 services.AddDbContext<ApplicationDbContext>(options =>
 //                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             }
